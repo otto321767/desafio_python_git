de274b767c86f9b20678f7d973c86f535a9ac5d9
desafio_python_git <ottofillipi185@gmail.com>
2025-09-05 14:18:02 -0300

    desafio_python_git

---
+++ src/app.py
@@ -0,0 +1,5 @@
+nome = ('otto')
+idade = ('15')
+linguagem_de_programacao_favorita = ('C++')
+
+print(f'Meu nome é {nome}, Minha idade é {idade}  é a  minha linguagem de programação favorita é {linguagem_de_programacao_favorita}')
