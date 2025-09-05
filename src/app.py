a76c55569543bd799e57d4d4fec8f5e501a52dc2
desafio_python_git <ottofillipi185@gmail.com>
2025-09-05 15:05:38 -0300

    desafio_python_git

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
-nome = ('otto')
-idade = ('15')
-linguagem_de_programacao_favorita = ('C++')
+nome = input('Qual o seu nome : ')
+idade = int(input('Qual a sua idade : '))
+linguagem_de_programacao_favorita = input ('Qual a sua linguagem de programação favorita : ')
 
 print(f'Meu nome é {nome}, Minha idade é {idade}  é a  minha linguagem de programação favorita é {linguagem_de_programacao_favorita}')
+ano_atual = 2025
+print(f'O ano de nacimento é {ano_atual - idade}' )
